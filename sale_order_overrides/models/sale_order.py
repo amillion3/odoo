9abafba0186174f57e1b398f29c1811fc92572ad
Andy Million <amillion@atiba.com>
2023-09-21 16:48:52 -0400

    remove methods to recalc totals beacuse these fields are not editable

--- sale_order_overrides/models/sale_order.py
+++ sale_order_overrides/models/sale_order.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
@@ -8,24 +8,24 @@ class SaleOrder(models.Model):
     user_amount_tax = fields.Monetary(currency_field="currency_id")
     user_amount_shipping = fields.Monetary(currency_field="currency_id")
 
-    @api.depends('order_line.tax_id', 'order_line.price_unit', 'amount_total', 'amount_untaxed', 'currency_id',
-                 "user_amount_shipping", "user_amount_tax")
-    def _compute_tax_totals(self):
-        for order in self:
-            order_lines = order.order_line.filtered(lambda x: not x.display_type)
-            tax_returned = self.env['account.tax']._prepare_tax_totals(
-                [x._convert_to_tax_base_line_dict() for x in order_lines],
-                order.currency_id or order.company_id.currency_id,
-            )
-            tax_returned['amount_untaxed'] += order.user_amount_tax
-            order.tax_totals = tax_returned
-
-    def _compute_shipping_totals(self):
-        for order in self:
-            order_lines = order.order_line.filtered(lambda x: not x.display_type)
-            tax_returned = self.env['account.tax']._prepare_tax_totals(
-                [x._convert_to_tax_base_line_dict() for x in order_lines],
-                order.currency_id or order.company_id.currency_id,
-            )
-            tax_returned['amount_untaxed'] += order.user_amount_tax
-            order.tax_totals = tax_returned
+    # @api.depends('order_line.tax_id', 'order_line.price_unit', 'amount_total', 'amount_untaxed', 'currency_id',
+    #              "user_amount_shipping", "user_amount_tax")
+    # def _compute_tax_totals(self):
+    #     for order in self:
+    #         order_lines = order.order_line.filtered(lambda x: not x.display_type)
+    #         tax_returned = self.env['account.tax']._prepare_tax_totals(
+    #             [x._convert_to_tax_base_line_dict() for x in order_lines],
+    #             order.currency_id or order.company_id.currency_id,
+    #         )
+    #         tax_returned['amount_untaxed'] += order.user_amount_tax
+    #         order.tax_totals = tax_returned
+    #
+    # def _compute_shipping_totals(self):
+    #     for order in self:
+    #         order_lines = order.order_line.filtered(lambda x: not x.display_type)
+    #         tax_returned = self.env['account.tax']._prepare_tax_totals(
+    #             [x._convert_to_tax_base_line_dict() for x in order_lines],
+    #             order.currency_id or order.company_id.currency_id,
+    #         )
+    #         tax_returned['amount_untaxed'] += order.user_amount_tax
+    #         order.tax_totals = tax_returned
