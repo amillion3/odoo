2dd293848f2d6d408178db3f01074241d89db353
Andy Million <amillion@atiba.com>
2023-10-11 15:52:44 -0400

    renamed XML and updated manifest

--- sale_order_overrides/__manifest__.py
+++ sale_order_overrides/__manifest__.py
@@ -3,14 +3,14 @@
 {
     'name': "Fast Partitions Overrides",
     'version': "1.0.2",
-    'depends': ["base", "account", "sale"],
+    'depends': ["base", "account", "account.move", "sale"],
     'author': "Atiba Software",
     'category': "contact",
     "license": "LGPL-3",
     'description': """""",
     'data': [
         "views/sale_order.xml",
-        "views/account_move.xml"
+        "views/account_move_views.xml"
     ],
     'installable': True,
 }
