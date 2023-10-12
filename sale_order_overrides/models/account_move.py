e365fc198b08f53c0b03d8b2e1df302e63552bea
Andy Million <amillion@atiba.com>
2023-10-12 08:55:44 -0400

    reverted field names in Model and trying new group name

--- sale_order_overrides/models/account_move.py
+++ sale_order_overrides/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    userrr_amount_fees = fields.Monetary(currency_field="currency_id", string="Order Fees/Discounts")
-    userrr_amount_tax = fields.Monetary(currency_field="currency_id", string="Order Taxes")
-    userrr_amount_shipping = fields.Monetary(currency_field="currency_id", string="Total Shipping")
-    userrr_amount_total = fields.Monetary(currency_field="currency_id", string="Order Total")
+    user_amount_fees = fields.Monetary(currency_field="currency_id", string="Order Fees/Discounts")
+    user_amount_tax = fields.Monetary(currency_field="currency_id", string="Order Taxes")
+    user_amount_shipping = fields.Monetary(currency_field="currency_id", string="Total Shipping")
+    user_amount_total = fields.Monetary(currency_field="currency_id", string="Order Total")
